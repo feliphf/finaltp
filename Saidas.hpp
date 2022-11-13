e25e0569bb7691ebf48f6caad9007db780ffacb1
anacarolinalimadesouza <112148991+anacarolinalimadesouza@users.noreply.github.com>
2022-11-13 15:24:06 -0300

    Update Saidas.hpp

--- Saidas.hpp
+++ Saidas.hpp
@@ -1,11 +1,11 @@
 /**
  * @file Entradas.hpp
  * @author Ana Carolina e Guilherme Pascoal
- * @brief Classe para retirar produtos do armazenada dos produtos.
+ * @brief Classe para retirar produtos armazenados na classe produto.
  * @version 1.0
  * @date 2022-11-13
- * @details A classe saídas está responsável por retirar produtos, 
- * que serão administados pela classe estoque.
+ * @details A classe saídas está responsável por retirar produtos da classe produto, 
+ * ao qual serão administados pela classe estoque.
  * @copyright GNU General Public License v2.0
  */
 
@@ -34,26 +34,26 @@ private:
 public:
 
 /**
-* @brief Função responsável por retirar um produto da lista de saídas.
-*
-* @details A partir da venda de um produto do estoque da empresa,
-* o funcionário irá retirar cada produto usando este método, a lista criá-da irá retirar um produto,
-* de acordo com o seu código, só poderá retirar, se já tiver sido adicionado ao estoque, além de tratar os erros,
-* que podem ser cometidos.
-*
-* @param código int do produto, de um produto existente no estoque da empresa.
-*/  
+ * @brief Função responsável por retirar um produto da lista de saídas. 
+ *
+ * @details A partir da venda de um produto do estoque da empresa,
+ * o funcionário irá retirar cada produto usando este método, a lista criá-da irá retirar um produto,
+ * de acordo com o seu código, só poderá retirar, se já tiver sido adicionado ao estoque, além de tratar os erros,
+ * que podem ser cometidos.
+ *
+ * @param código int do produto, de um produto existente no estoque da empresa.
+ */  
   void retiraProduto(int código);
     
 /**
-* @brief Possibilita a consulta do funcionário, para conferir se retirou corretamente
-* o produto que gostaria de retirar, conferindo-o, pelo código do produto.
-*
-* @details Além de possibilitar a consulta, e a conferência, ele também trata os erros,
-* que podem ser cometidos.
-
-* @param código int, o código do produto, ao qual foi retirado nas saídas.
-*/
+ * @brief Possibilita a consulta do funcionário, para conferir se retirou corretamente
+ * o produto que gostaria de retirar, conferindo-o, pelo código do produto.
+ *
+ * @details Além de possibilitar a consulta, e a conferência, ele também trata os erros,
+ * que podem ser cometidos.
+ *
+ * @param código int, o código do produto, ao qual foi retirado nas saídas.
+ */
   std::string consultarRetirados (int codigo);
   
 };
