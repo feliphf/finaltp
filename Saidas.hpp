71e605205677b6dd5e6c7e0391747cbd84e806c2
guipascoall <112048220+guipascoall@users.noreply.github.com>
2022-11-13 15:28:09 -0300

    Update Saidas.hpp

--- Saidas.hpp
+++ Saidas.hpp
@@ -20,11 +20,12 @@
 /**
  * @brief Retirada de um produto em uma lista de saídas.
  *
- * @details A classe saídas está responsável por retirar produtos de acordo com as vendas feitas na empresa, 
- * retirando-as, pela classe de cátologo produto, desta forma, irá retirar cada produto,  
- * ela também retorna a consulta de determinado produto, 
- * para ser conferido se realmente foi retirado.
+ * @details A classe saídas está responsável por retirar produtos de acordo com as  
+ * vendas feitas na empresa, retirando-as, pela classe de cátologo produto, desta  
+ * forma, irá retirar cada produto, ela também retorna a consulta de determinado 
+ * produto, para ser conferido se realmente foi retirado.
  */
+
 class Saidas {
 private:
     
@@ -36,10 +37,10 @@ public:
 /**
  * @brief Função responsável por retirar um produto da lista de saídas. 
  *
- * @details A partir da venda de um produto do estoque da empresa,
- * o funcionário irá retirar cada produto usando este método, a lista criá-da irá retirar um produto,
- * de acordo com o seu código, só poderá retirar, se já tiver sido adicionado ao estoque, além de tratar os erros,
- * que podem ser cometidos.
+ * @details A partir da venda de um produto do estoque da empresa, o funcionário 
+ * irá retirar cada produto usando este método, a lista criá-da irá retirar um 
+ * produto, de acordo com o seu código, só poderá retirar, se já tiver sido adicionado 
+ * ao estoque, além de tratar os erros, que podem ser cometidos.
  *
  * @param código int do produto, de um produto existente no estoque da empresa.
  */  
@@ -49,8 +50,8 @@ public:
  * @brief Possibilita a consulta do funcionário, para conferir se retirou corretamente
  * o produto que gostaria de retirar, conferindo-o, pelo código do produto.
  *
- * @details Além de possibilitar a consulta, e a conferência, ele também trata os erros,
- * que podem ser cometidos.
+ * @details Além de possibilitar a consulta, e a conferência, ele também trata os 
+ * erros, que podem ser cometidos.
  *
  * @param código int, o código do produto, ao qual foi retirado nas saídas.
  */
