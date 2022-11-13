52934ff62b1f1531220eda7ecdd980bda620e53c
anacarolinalimadesouza <112148991+anacarolinalimadesouza@users.noreply.github.com>
2022-11-13 14:09:18 -0300

    Update Saidas.hpp

--- Saidas.hpp
+++ Saidas.hpp
@@ -1,1 +1,20 @@
+#ifndef SAIDA_HPP
+#define SAIDA_HPP
 
+#include <iostream>
+#include <list>
+
+#include "produto.hpp"
+
+
+class Saidas {
+private:
+    
+  std::list<Produto*> saidas;
+  
+public:
+
+  void retiraProduto(Produto* p);
+  
+};
+#endif
