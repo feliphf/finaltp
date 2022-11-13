a536b58b761e2966efbd89dc4510ae18eb6af48d
guipascoall <112048220+guipascoall@users.noreply.github.com>
2022-11-13 14:46:54 -0300

    Update Produto.hpp

--- Produto.hpp
+++ Produto.hpp
@@ -3,20 +3,36 @@
 #include <iostream>
 
 /**
- * @brief Classe base responsavel pelo armazenamento e manipulacao das
- * informacoes basicas de um produto.
+ * @file Produto.hpp
+ * @author Ana Carolina e Guilherme Pascoal
+ * @brief Classe para armazenamento dos produtos
+ * @version 1.0
+ * @date 2022-11-13
+ * @details A classe produto está responsável por armazenar os atributos necessários para os produtos, 
+ * servindo como uma espécie de catálogo onde o funcionário ou gerente poderá consultar a disponibilidade
+ * de um produto e adcionar/retirar um item do catálogo. 
+ * @copyright GNU General Public License v2.0
  */
 
 class Produto{
   
-  /**
-   * @brief Atributos do produto
-   */
   private:
   
+  /**
+   * @brief Código único do produto.
+   */
   int codigo;
+  /**
+   * @brief Preço unitário do produto.
+   */
   float preco;
+  /**
+   * @brief Nome do produto.
+   */
   std::string nome;
+  /**
+   * @brief Categoria do produto.
+   */
   std::string categoria;
   std::string fabricante;
   
