3f62a195a3e36f504376cd36c47338cf9d1afd17
guipascoall <112048220+guipascoall@users.noreply.github.com>
2022-11-13 13:37:38 -0300

    Update Entradas.hpp

--- Entradas.hpp
+++ Entradas.hpp
@@ -1,1 +1,15 @@
+# ifndef ENTRADAS.HPP
+# define ENTRADAS.HPP
+# include <iostream>
 
+class Produto{
+  private:
+  entradas list <Produto *>;
+    
+  public:
+  list get_entrada ();
+  consultarAdcionar (int codigo){
+    
+  };
+};
+#endif;
