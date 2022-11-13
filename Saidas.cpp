6944aededecd242662c37a251154227ac3b8b762
guipascoall <112048220+guipascoall@users.noreply.github.com>
2022-11-13 13:42:25 -0300

    Update Saidas.cpp

--- Saidas.cpp
+++ Saidas.cpp
@@ -1,1 +1,15 @@
+# ifndef SAIDAS.HPP
+# define SAIDAS.HPP
+# include <iostream>
 
+class Produto{
+  private:
+  saidas list <Produto *>;
+    
+  public:
+  list get_saidas ();
+  consultarRetirar (int codigo){
+  
+  };
+};
+#endif;
