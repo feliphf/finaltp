79272204432cad187b4b77c7e92338b64f173970
anacarolinalimadesouza <112148991+anacarolinalimadesouza@users.noreply.github.com>
2022-11-13 15:41:13 -0300

    Update Entradas.cpp

--- Entradas.cpp
+++ Entradas.cpp
@@ -1,1 +1,12 @@
+#include "Produto.hpp"
+#include "Entradas.hpp"
+#include <iostream>
+#include <list>
+
+void Entradas::adicionaProduto(Produto* p){
+  }
+std::list Entradas::consultarAdicionado (int codigo){
+  }
+    
+}
 
