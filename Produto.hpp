42f3af3cfe2611c470096af7829c1fb0391ad7bd
guipascoall <112048220+guipascoall@users.noreply.github.com>
2022-11-13 13:31:13 -0300

    Update Produto.hpp

--- Produto.hpp
+++ Produto.hpp
@@ -1,2 +1,26 @@
 # indef PRODUTO.HPP
 # define PRODUTO.HPP
+# include <iostream>
+
+class Produto{
+  private:
+  string nome;
+  int codigo;
+  string fabricante;
+  float preco;
+  string categoria;
+  
+  public:
+  string get_nome ();
+  int get_codigo ();
+  string get_fabricante ();
+  float get_preco ();
+  string get_categoria ();
+  void set_nome ();
+  void set_codigo ();
+  void set_fabricante ();
+  void set_preco ();
+  void get_categoria ();
+};
+#endif;
+  
