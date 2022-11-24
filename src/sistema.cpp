e7df4e57c52bcbef475f2420337cb5f3e0a46a02
yanfigueiras <yanfigueirasalves@gmail.com>
2022-11-24 01:18:50 -0300

    novas funções

--- src/sistema.cpp
+++ src/sistema.cpp
@@ -12,8 +12,42 @@ void Sistema::menuInicial(){
                                                                                   
 )" << std::endl;
     std::cout << "Escolha uma opcao: " << std::endl;
-	std::cout << "+-----------------+"<<std::endl;
-	std::cout << "| " << "1 - Fazer Login |" << std::endl;
-	std::cout << "| " << "2 - Sair        |" << std::endl;
-	std::cout << "+-----------------+"<<std::endl;
+	std::cout << "+-----------------------------+"<<std::endl;
+	std::cout << "| " << "1 - Fazer Login       |" << std::endl;
+	std::cout << "| " << "2 - Sair              |" << std::endl;
+	std::cout << "+-----------------------------+"<<std::endl;
 }
+
+void Sistema::menuFuncionario(){
+    std::cout << "Escolha uma opcao: " << std::endl;
+	std::cout << "+-----------------------------+"<<std::endl;
+	std::cout << "| " << "1 - Consultar Produto |" << std::endl;
+	std::cout << "| " << "2 - Listar Produtos   |" << std::endl;
+	std::cout << "| " << "3 - Registrar Venda   |" << std::endl;
+	std::cout << "| " << "4 - Sair              |" << std::endl;
+	std::cout << "+-----------------------------+"<<std::endl;
+}
+
+void Sistema::menuGerente(){
+    std::cout << "Escolha uma opcao: " << std::endl;
+	std::cout << "+-----------------------------+"<<std::endl;
+	std::cout << "| " << "1 - Consultar Produto |" << std::endl;
+	std::cout << "| " << "2 - Listar Produtos   |" << std::endl;
+	std::cout << "| " << "3 - Registrar Venda   |" << std::endl;
+	std::cout << "| " << "3 - Registrar Compra  |" << std::endl;
+	std::cout << "| " << "3 - Alterar Produto   |" << std::endl;
+	std::cout << "| " << "4 - Sair              |" << std::endl;
+	std::cout << "+-----------------------------+"<<std::endl;
+}
+
+void Sistema::menuAlterarProduto(){
+    std::cout << "Escolha o que deseja alterar: " << std::endl;
+	std::cout << "+-----------------------------+"<<std::endl;
+	std::cout << "| " << "1 - Nome              |" << std::endl;
+	std::cout << "| " << "2 - Categoria         |" << std::endl;
+	std::cout << "| " << "3 - Fabricante        |" << std::endl;
+	std::cout << "| " << "3 - Preço             |" << std::endl;
+	std::cout << "| " << "4 - Sair              |" << std::endl;
+	std::cout << "+-----------------------------+"<<std::endl;
+}
+
