2258f7140f6b20850f8c46be179c3f110b139c87
anacarolinalimadesouza <112148991+anacarolinalimadesouza@users.noreply.github.com>
2022-12-02 23:14:36 -0300

    Update EXCECOESDOLOGIN.hpp

--- include/login/EXCECOESDOLOGIN.hpp
+++ include/login/EXCECOESDOLOGIN.hpp
@@ -1,1 +1,41 @@
+#ifndef _EXCECOESDOLOGIN_HPP_
+#define _EXCECOESDOLOGIN_HPP_
+#include "exception.hpp"
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+class UsuarioNaoCadastrado : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		UsuarioNaoCadastrado();
+		virtual const char* what() const throw();
+};
+
+class SenhaIncorreta : public std::exception(){
+	private:
+		std::string mensagem;
+    public:
+		SenhaIncorreta();
+		virtual const char* what() const throw();
+};
+
+class EntradaLoginInvalida : public std::exception(){
+	private:
+		std::string mensagem;
+    public:
+		EntradaLoginInvalida();
+		virtual const char* what() const throw();
+};
+
+class EscolhaInvalida : public std::exception(){
+	private:
+		std::string mensagem;
+    public:
+		EscolhaInvalida();
+		virtual const char* what() const throw();
+};
+
+#endif
