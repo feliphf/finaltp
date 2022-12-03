f04d8d510bc5847d98d4488b8911a0df1793519e
guipascoall <112048220+guipascoall@users.noreply.github.com>
2022-12-02 22:23:51 -0300

    Update EXCECOESDOESTOQUE.hpp

--- include/estoque/EXCECOESDOESTOQUE.hpp
+++ include/estoque/EXCECOESDOESTOQUE.hpp
@@ -1,1 +1,29 @@
+#include "EXCECOESDOESTOQUE.hpp"
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+ProdutoNaoCadastrado::ProdutoNaoCadastrado(){
+	mensagem = "Produto não cadastrado";	
+}
+
+virtual const char* ProdutoNaoCadastrado::what() const throw(){
+	return memsagem.c_str();
+}
+
+PrecoNaoENumero::PrecoNaoENumero(){
+	mensagem = "Preço selecionado para alteração não é válido";	
+}
+
+virtual const char* PrecoNaoENumero::what() const throw(){
+	return memsagem.c_str();
+}
+
+EntradaInválida::EntradaInvalida(){
+	mensagem = "Entrada inválida, digite um número de 1 a 5";
+}
+
+virtual const char* EntradaInvalida::what() const throw(){
+	return memsagem.c_str();
+}
