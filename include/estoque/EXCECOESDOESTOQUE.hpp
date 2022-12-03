f9c022b62e74281e1c2f503c4bd3e85d51148639
anacarolinalimadesouza <112148991+anacarolinalimadesouza@users.noreply.github.com>
2022-12-03 17:53:57 -0300

    Update EXCECOESDOESTOQUE.hpp

--- include/estoque/EXCECOESDOESTOQUE.hpp
+++ include/estoque/EXCECOESDOESTOQUE.hpp
@@ -28,5 +28,69 @@ class EntradaInvalida : public std::exception{
 		virtual const char* what() const throw();
 };
 
-#endif
+class ExcecaoTamanhoNomeAlt : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		ExcecaoTamanhoNomeAlt();
+		virtual const char* what() const throw();
+};
 
+class ExcecaoTamanhoFabricAlt : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		ExcecaoTamanhoFabricAlt();
+		virtual const char* what() const throw();
+};
+
+class ExcecaoTamanhoCategAlt : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		ExcecaoTamanhoCategAlt();
+		virtual const char* what() const throw();
+};
+
+class ExcecaoPrecoNegativoAlt : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		EntradaInvalida();
+		virtual const char* what() const throw();
+};
+
+class ProdutoJaCadastrado : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		ProdutoJaCadastrado();
+		virtual const char* what() const throw();
+};
+
+class ProdutoNaoAdicionado : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		ProdutoNaoAdicionado();
+		virtual const char* what() const throw();
+};
+
+class ProdutoNaoRetirado : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		ProdutoNaoRetirado();
+		virtual const char* what() const throw();
+};
+
+class TentativaDeRetirada : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		TentativaDeRetirada();
+		virtual const char* what() const throw();
+};
+
+
+#endif
