44a3bc0fa614102ce2469dba6926b4936853d6c7
guipascoall <112048220+guipascoall@users.noreply.github.com>
2022-12-02 22:22:55 -0300

    Update EXCECOESDOESTOQUE.cpp

--- src/estoque/EXCECOESDOESTOQUE.cpp
+++ src/estoque/EXCECOESDOESTOQUE.cpp
@@ -1,1 +1,33 @@
+#ifndef _EXCECOESDOESTOQUE_HPP_
+#define _EXCECOESDOESTOQUE_HPP_
+#include "exception.hpp"
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+class ProdutoNaoCadastrado : public std::exception{
+	private:
+		string mensagem;
+    public:,
+		ProdutoNaoCadastrado();
+		virtual const char* what() const throw();
+};
+
+class PrecoNaoENumero : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		PrecoNaoENumero();
+		virtual const char* what() const throw();
+};
+
+class EntradaInvalida : public std::exception{
+	private:
+		std::string mensagem;
+    public:
+		EntradaInvalida();
+		virtual const char* what() const throw();
+};
+
+#endif
