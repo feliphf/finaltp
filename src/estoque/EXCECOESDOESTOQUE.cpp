6077760588424b5e3a73e1294467f37f4b0864cf
anacarolinalimadesouza <112148991+anacarolinalimadesouza@users.noreply.github.com>
2022-12-03 18:30:39 -0300

    Update EXCECOESDOESTOQUE.cpp

--- src/estoque/EXCECOESDOESTOQUE.cpp
+++ src/estoque/EXCECOESDOESTOQUE.cpp
@@ -13,18 +13,81 @@ virtual const char* ProdutoNaoCadastrado::what() const throw(){
 }
 
 PrecoNaoENumero::PrecoNaoENumero(){
-	mensagem = "Preço selecionado para alteração não é válido";	
+	mensagem = "Preço inválido, digite novamente";	
 }
 
 virtual const char* PrecoNaoENumero::what() const throw(){
 	return mensagem.c_str();
 }
 
-EntradaInválida::EntradaInvalida(){
-	mensagem = "Escolha inválida, digite um número de 1 a 5";
+EntradaInvalida::EntradaInvalida(){
+	mensagem = "Escolha inválida, digite novamente";
 }
 
 virtual const char* EntradaInvalida::what() const throw(){
 	return mensagem.c_str();
 }
 
+ExcecaoTamanhoNomeAlt::ExcecaoTamanhoNomeAlt(){
+	mensagem = "Nome escolhido maior que o esperado, digite novamente o nome que deseja, com até 20 caracteres";
+}
+
+virtual const char* ExcecaoTamanhoNomeAlt::what() const throw(){
+	return mensagem.c_str();
+}
+
+ExcecaoTamanhoFabricAlt::ExcecaoTamanhoFabricAlt(){
+	mensagem = "Fabricante escolhido maior que o esperado, digite novamente o fabricante que deseja, com até 20 caracteres";
+}
+
+virtual const char* ExcecaoTamanhoFabricAlt::what() const throw(){
+	return mensagem.c_str();
+}
+
+ExcecaoTamanhoCategAlt::ExcecaoTamanhoCategAlt(){
+	mensagem = "Categoria escolhida maior que o esperado, digite novamente a categoria que deseja, com até 20 caracteres";
+}
+
+virtual const char* ExcecaoTamanhoCategAlt::what() const throw(){
+	return mensagem.c_str();
+}
+
+ExcecaoPrecoNegativoAlt::ExcecaoPrecoNegativoAlt(){
+	mensagem = "Preço inválido, digite novamente";
+}
+
+virtual const char* ExcecaoPrecoNegativoAlt::what() const throw(){
+	return mensagem.c_str();
+}
+
+ProdutoJaCadastrado::ProdutoJaCadastrado(){
+	mensagem = "Produto já cadastrado, por favor adicione a quantidade no campo de entradas";
+}
+
+virtual const char* ProdutoJaCadastrado::what() const throw(){
+	return mensagem.c_str();
+}
+
+ProdutoNaoAdicionado::ProdutoNaoAdicionado(){
+	mensagem = "Produto não adicionado, quantidade pedida igual a 0";
+}
+
+virtual const char* ProdutoNaoAdicionado::what() const throw(){
+	return mensagem.c_str();
+}
+
+ProdutoNaoRetirado::ProdutoNaoRetirado(){
+	mensagem = "Produto não retirado, quantidade pedida igual a 0";
+}
+
+virtual const char* ProdutoNaoRetirado::what() const throw(){
+	return mensagem.c_str();
+}
+
+TentativaDeRetirada::TentativaDeRetirada(){
+	mensagem = "Tentativa de retirada abaixo da quantidade disponível em estoque";
+}
+
+virtual const char* TentativaDeRetirada::what() const throw(){
+	return mensagem.c_str();
+}
