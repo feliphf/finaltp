66ae4fd3b2cf60b477dda09aaa4bf707e827ce03
guipascoall <112048220+guipascoall@users.noreply.github.com>
2022-12-05 20:48:27 -0300

    Update funcionario_test.cpp

--- TEST/funcionario_test.cpp
+++ TEST/funcionario_test.cpp
@@ -4,51 +4,15 @@
 
 TEST_CASE("Testando a função menu inicial")
 {
-    //EXEMPLOS DE 3 CHECAGENS
-    Triangle t1(3,2,2);
-    CHECK(t1.getPerimeter()==7);
-
-    Usuario usuario = Usuario();
-	usuario.setCpf("12341234123");
-	usuario.setSenha("senha");
-	CHECK(usuario.login());
-    
-    Usuario usuario = Usuario();
-	usuario.setCpf("32132132132");
-	usuario.setSenha("senha");
-	CHECK_THROWS_AS(usuario.login(), UsuarioNaoEncontradoException);
+    Funcionario f1 ("1029", "Funcionário", "Ana Carolina", "82392354322", "anacarol@gmail.com", "dia 05 de novembro", "239845");
+    CHECK (f1.menuInicial());
+	
+    Funcionario f2 ("2342", "Funcionário", "Gabriel Lage", "47392839273", "bielage@gmail.com", "dia 04 de dezembro", "593023");
+    CHECK (f2.menuInicial());
 }
 
 TEST_CASE("Testando a função se o usuário tem permissão para determinadas funções") 
 {
-    //EXEMPLOS DE 3 CHECAGENS
-    Triangle t1(3,2,2);
-    CHECK(t1.getPerimeter()==7);
-
-    Usuario usuario = Usuario();
-	usuario.setCpf("12341234123");
-	usuario.setSenha("senha");
-	CHECK(usuario.login());
-    
-    Usuario usuario = Usuario();
-	usuario.setCpf("32132132132");
-	usuario.setSenha("senha");
-	CHECK_THROWS_AS(usuario.login(), UsuarioNaoEncontradoException);
-}
-
-TEST_CASE("Testando a função que altera o usuário logado") 
-{
-    //EXEMPLOS DE 3 CHECAGENS
-    Triangle t1(3,2,2);
-    CHECK(t1.getPerimeter()==7);
-
-    Usuario usuario = Usuario();
-	usuario.setCpf("12341234123");
-	usuario.setSenha("senha");
-	CHECK(usuario.login());
-    
-    Usuario usuario = Usuario();
-	usuario.setCpf("32132132132");
-	usuario.setSenha("senha");
-	CHECK_THROWS_AS(usuario.login(), UsuarioNaoEncontradoException);
+    Funcionario f1("1029", "Funcionário", "Ana Carolina", "82392354322", "anacarol@gmail.com", "dia 05 de novembro", "239845");
+    CHECK (f1.temPermissao() == false);
 }
