8a4f91080d003468e26b07f5af135dc20e066323
guipascoall <112048220+guipascoall@users.noreply.github.com>
2022-12-05 23:13:30 -0300

    Update usuario.hpp

--- include/login/usuario.hpp
+++ include/login/usuario.hpp
@@ -29,39 +29,107 @@ using namespace std;
    * 
    */
 
-
 class Usuario {
     protected:
+  
+        ///@brief
         string _id;
+  
+        ///@brief
         string _tipo;
+  
+        ///@brief
         string _nome;
+  
+        ///@brief
         string _cpf;
+  
+        ///@brief
         string _email;
+  
+        ///@brief
         string _ultimoAcesso;
+  
+        ///@brief
         string _senha;
+  
+        ///@brief
         bool _estaAutenticado;
+  
     public:
+  
+        ///@brief 
+        ///@return
         string getId();
+  
+        ///@brief 
+        ///@return
         string getTipo();
+  
+        ///@brief 
+        ///@return
         string getNome();
+  
+        ///@brief 
+        ///@return
         string getCpf();
+  
+        ///@brief 
+        ///@return
         string getEmail();
+  
+        ///@brief 
+        ///@return
         string getUltimoAcesso();
+  
+        ///@brief 
+        ///@return
         string getSenha();
+  
+        ///@brief 
+        ///@return
         bool getEstaAutenticado();
+  
+        ///@brief
         void setId(string s);
+  
+        ///@brief
         void setTipo(string s);
+  
+        ///@brief
         void setNome(string s);
+  
+        ///@brief
         void setCpf(string s);
+  
+        ///@brief
         void setEmail(string s);
+  
+        ///@brief
         void setUltimoAcesso(string s);
+  
+        ///@brief
         void setSenha(string s);
+  
+        ///@brief
         void setEstaAutenticado(bool b);
+  
+        ///@brief
         void alterarUsuario();
+  
+        ///@brief
         virtual void menuAdministrativo() = 0;
+  
+        ///@brief
         virtual void alterarUsuarioLogado() = 0;
+  
+        ///@brief
         virtual void menuInicial() = 0;
+  
+        ///@brief
         virtual bool temPermissao() = 0;
+  
+        ///@brief
         void limparTela(string modo);
 };
 
